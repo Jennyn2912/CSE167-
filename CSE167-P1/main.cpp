da7ca36cdc1672bd735baebbabd12799a507cca9
Jenny Nguyen <ttn053@ucsd.edu>
2016-09-29 02:43:58 -0700

    Initial Commit

---
+++ CSE167-P1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE167-P1
+//
+//  Created by Jenny Nguyen on 9/29/16.
+//  Copyright © 2016 jennyn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
